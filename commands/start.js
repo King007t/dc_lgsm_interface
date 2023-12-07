1a3798d1f500013ffb77c9263ef40a2606464cd0
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 00:17:48 +0100

    Added feedback to discord after command execution

--- commands/start.js
+++ commands/start.js
@@ -10,13 +10,30 @@ module.exports = {
             });
       
       const script = exec(serverpath + ' start');
+      var boolean = false;
+      var output = "\n**Output:**``";
+      var erroutput = "\n**Error:**``";
 
       script.stdout.on('data', data => {
-         console.log(data);
+	 console.error(data);
+	 output += data;
       });
 
       script.stderr.on('data', data => {
          console.error(data);
+	 erroutput += data;
+	 boolean = true;
       });
+
+      script.on('exit', function() {
+	if(boolean) {
+    	 message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
+               setTimeout(() => msg.delete(), msgsec * 1000);
+            });
+	} else {
+	 message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
+               setTimeout(() => msg.delete(), msgsec * 1000);
+            });
+	}});
    },
 };
