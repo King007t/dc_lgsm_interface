4733ec74d63b217075df18477fa1741bbe1400d6
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 01:29:24 +0100

    Update backup.js

--- commands/backup.js
+++ commands/backup.js
@@ -2,38 +2,38 @@ const { exec } = require('child_process');
 const { serverpath, msgsec } = require('../config.json');
 
 module.exports = {
-   name: 'backup',
-   description: 'Backs up the LinuxGSM server',
-   execute(message, args) {
-      message.channel.send("Backing up the server! :package:").then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
-            });
+	name: 'backup',
+	description: 'Backs up the LinuxGSM server',
+	execute(message, args) {
+		message.channel.send("Backing up the server! :package:").then(msg => {
+			setTimeout(() => msg.delete(), msgsec * 1000);
+		});
       
-      const script = exec(serverpath + ' backup');
-      var boolean = false;
-      var output = "\n**Output:**``";
-      var erroutput = "\n**Error:**``";
+      	const script = exec(serverpath + ' backup');
+      	var boolean = false;
+      	var output = "\n**Output:**``";
+      	var erroutput = "\n**Error:**``";
 
-      script.stdout.on('data', data => {
-	 console.error(data);
-	 output += data;
-      });
+      	script.stdout.on('data', data => {
+		console.error(data);
+		output += data;
+      	});
 
-      script.stderr.on('data', data => {
-         console.error(data);
-	 erroutput += data;
-	 boolean = true;
-      });
+      	script.stderr.on('data', data => {
+        	console.error(data);
+			erroutput += data;
+	 		boolean = true;
+      	});
 
-      script.on('exit', function() {
-	if(boolean) {
-    	 message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
+      	script.on('exit', function() {
+		if(boolean) {
+    	 	message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
+              	 setTimeout(() => msg.delete(), msgsec * 1000);
             });
-	} else {
-	 message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
-            });
-	}});
-   },
+		} else {
+	 		message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
+       			setTimeout(() => msg.delete(), msgsec * 1000);
+        	});
+		}});
+   	},
 };
