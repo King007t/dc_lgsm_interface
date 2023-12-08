e8f3be45e5eb162f1c0b99688c45181cbf7de8ff
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 02:12:40 +0100

    ability to lock after command

--- events/message.js
+++ events/message.js
@@ -1,54 +1,56 @@
 const { prefix, required_role, msgsec } = require('../config.json');
 
 module.exports = {
-   name: 'message',
-   execute(message, client) {
-      if (!message.content.startsWith(prefix) || message.author.bot) return;
-      message.delete();
-      // If there's a required role, check that the user sending the command has that role
-      if (required_role.use && !message.member.roles.cache.some(role => role.name === required_role.name)) {
-         message.reply(`Sorry, you don't have the right privileges. You can ask a Staff member for help.`).then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
-            });
-         return;
-      }
-      
-      const { locked } = require("../config.json");
-      
-      if (locked) {
-         message.reply(`Please wait for the previous command to execute! :warning:`).then(msg => {
-            setTimeout(() => msg.delete(), msgsec * 1000);
-         });
-         return;
-      }
-      
-      const args = message.content.slice(prefix.length).trim().split(/ +/);
-      const commandName = args.shift().toLowerCase();
+	name: 'message',
+	execute(message, client) {
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+		message.delete();
 
-      if (!client.commands.has(commandName)) {
-         message.reply(`No command found. Use \`\`${prefix}help\`\` for available commands`).then(msg => {
-            setTimeout(() => msg.delete(), msgsec * 1000);
-         });
-         return;
-      }
-      
-      if(commandName != "setchannel") {
-         const { channel } = require("../config.json");
-         if(message.channel.id != config.channel) {
-            message.reply(`Please use the correct channel`).then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
-            });
-            return;
-         }
-      }
+		// If there's a required role, check that the user sending the command has that role
 
-      try {
-         client.commands.get(commandName).execute(message, args);
-      } catch (error) {
-         console.error(error);
-         message.reply('There was an error trying to execute that command!').then(msg => {
-               setTimeout(() => msg.delete(), msgsec * 1000);
-            });
-      }
-   }
+		if (required_role.use && !message.member.roles.cache.some(role => role.name === required_role.name)) {
+			message.reply(`Sorry, you don't have the right privileges. You can ask a Staff member for help.`).then(msg => {
+				setTimeout(() => msg.delete(), msgsec * 1000);
+			});
+			return;
+		}
+
+		const { locked } = require("../config.json");
+
+		if (locked) {
+			message.reply(`Please wait for the previous command to execute! :warning:`).then(msg => {
+				setTimeout(() => msg.delete(), msgsec * 1000);
+			});
+			return;
+		}
+
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		const commandName = args.shift().toLowerCase();
+
+		if (!client.commands.has(commandName)) {
+			message.reply(`No command found. Use \`\`${prefix}help\`\` for available commands`).then(msg => {
+				setTimeout(() => msg.delete(), msgsec * 1000);
+			});
+			return;
+		}
+ 
+		if(commandName != "setchannel") {
+			const { channel } = require("../config.json");
+			if(message.channel.id != config.channel) {
+				message.reply(`Please use the correct channel`).then(msg => {
+					setTimeout(() => msg.delete(), msgsec * 1000);
+				});
+				return;
+			}
+		}
+
+		try {
+			client.commands.get(commandName).execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('There was an error trying to execute that command!').then(msg => {
+				setTimeout(() => msg.delete(), msgsec * 1000);
+			});
+		}
+	}
 };
--- events/ready.js
+++ events/ready.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const config = require("../config.json");
 
 module.exports = {
-      name: 'ready',
-      once: true,
-      execute(client) {
-         config.locked = false;
-      
-         fs.writeFile(__dirname.replace('events', '') + "config.json", JSON.stringify(config), function writeJSON(err) {
-            if (err) return console.log(err);
-         });
-      
-         console.log(`${client.user.tag} HAS ARRIVED!`);
-         console.log(`(The bot is running, connected to your Discord and ready to be used)`);
-      }
+	name: 'ready',
+	once: true,
+	execute(client) {
+		config.locked = false;
+
+		fs.writeFile(__dirname.replace('events', '') + "config.json", JSON.stringify(config), function writeJSON(err) {
+			if (err) return console.log(err);
+		});
+
+		console.log(`${client.user.tag} HAS ARRIVED!`);
+		console.log(`(The bot is running, connected to your Discord and ready to be used)`);
+	}
 };
