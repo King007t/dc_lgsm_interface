37f29281ab43022c05346e74f99afdcd838c8f4c
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 01:43:35 +0100

    Add files via upload

--- commands/backup.js
+++ commands/backup.js
@@ -10,18 +10,18 @@ module.exports = {
 			setTimeout(() => msg.delete(), config.msgsec * 1000);
 		});
       
-      	const script = exec(config.serverpath + ' backup');
-     	var boolean = false;
-    	var output = "\n**Output:**``";
-    	var erroutput = "\n**Error:**``";
+      		const script = exec(config.serverpath + ' backup');
+     		var boolean = false;
+    		var output = "\n**Output:**``";
+    		var erroutput = "\n**Error:**``";
 
-  	    script.stdout.on('data', data => {
+  	    	script.stdout.on('data', data => {
 			console.error(data);
 			output += data;
    	   	});
 
 		script.stderr.on('data', data => {
-    		console.error(data);
+    			console.error(data);
 			erroutput += data;
 		 	boolean = true;
    	   	});
@@ -30,17 +30,17 @@ module.exports = {
 			config.locked = false;
       
 			fs.writeFile(__dirname.replace('events', '') + "config.json", JSON.stringify(config), function writeJSON(err) {
-    			if (err) return console.log(err);
-    		});
+    				if (err) return console.log(err);
+    			});
 		
 			if(boolean) {
-    			message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
+    				message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
 					setTimeout(() => msg.delete(), config.msgsec * 1000);
-    	        });
+    	        		});
 			} else {
 		 		message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
  	      			setTimeout(() => msg.delete(), config.msgsec * 1000);
-	        	});
+	        		});
 			}
 		});
 	},
