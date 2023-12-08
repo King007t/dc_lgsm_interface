27e0003faf3f312813876fd72e323d2ee2307944
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 01:35:42 +0100

    lock

--- commands/backup.js
+++ commands/backup.js
@@ -1,15 +1,16 @@
 const { exec } = require('child_process');
-const { serverpath, msgsec } = require('../config.json');
+const fs = require('fs');
+const config = require("../config.json");
 
 module.exports = {
 	name: 'backup',
 	description: 'Backs up the LinuxGSM server',
 	execute(message, args) {
 		message.channel.send("Backing up the server! :package:").then(msg => {
-			setTimeout(() => msg.delete(), msgsec * 1000);
+			setTimeout(() => msg.delete(), config.msgsec * 1000);
 		});
       
-      	const script = exec(serverpath + ' backup');
+      	const script = exec(config.serverpath + ' backup');
       	var boolean = false;
       	var output = "\n**Output:**``";
       	var erroutput = "\n**Error:**``";
@@ -26,14 +27,21 @@ module.exports = {
       	});
 
       	script.on('exit', function() {
-		if(boolean) {
-    	 	message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
-              	 setTimeout(() => msg.delete(), msgsec * 1000);
-            });
-		} else {
-	 		message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
-       			setTimeout(() => msg.delete(), msgsec * 1000);
-        	});
-		}});
+			config.locked = false;
+      
+    		fs.writeFile(__dirname.replace('events', '') + "config.json", JSON.stringify(config), function writeJSON(err) {
+    			if (err) return console.log(err);
+    		});
+		
+			if(boolean) {
+    	 		message.channel.send("An error has accured! Check the console output. :warning:  \n" + erroutput + "``").then(msg => {
+            	  	 setTimeout(() => msg.delete(), config.msgsec * 1000);
+            	});
+			} else {
+	 			message.channel.send("The command has been executed succesfully! :white_check_mark: \n" + output + "``").then(msg => {
+       				setTimeout(() => msg.delete(), config.msgsec * 1000);
+        		});
+			}
+		});
    	},
 };
