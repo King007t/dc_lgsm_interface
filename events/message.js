ba1cebf5a921e571c041ed0fe0d880063380d01f
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 01:16:35 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,24 +15,26 @@ module.exports = {
       
       const { locked } = require("../config.json");
       
-      if (locked) 
+      if (locked) {
          message.reply(`Please wait for the previous command to execute! :warning:`).then(msg => {
             setTimeout(() => msg.delete(), msgsec * 1000);
          });
          return;
+      }
       
       const args = message.content.slice(prefix.length).trim().split(/ +/);
       const commandName = args.shift().toLowerCase();
 
-      if (!client.commands.has(commandName)) 
+      if (!client.commands.has(commandName)) {
          message.reply(`No command found. Use \`\`${prefix}help\`\` for available commands`).then(msg => {
             setTimeout(() => msg.delete(), msgsec * 1000);
          });
          return;
-   
-      if(commandName != "setchannel"){
+      }
+      
+      if(commandName != "setchannel") {
          const { channel } = require("../config.json");
-         if(message.channel.id != config.channel){
+         if(message.channel.id != config.channel) {
             message.reply(`Please use the correct channel`).then(msg => {
                setTimeout(() => msg.delete(), msgsec * 1000);
             });
