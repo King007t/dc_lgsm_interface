0449c6919273c73f0562c9052ebc2c9786a9c0e3
King007t <119011004+King007t@users.noreply.github.com>
2023-12-08 02:40:42 +0100

    bugfix

--- bot.js
+++ bot.js
@@ -10,21 +10,23 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync(__dirname + '/commands/').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-   const command = require(`./commands/${file}`);
-   client.commands.set(command.name, command)
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command)
 }
 
 // Load event files
+
 const eventFiles = fs.readdirSync(__dirname + '/events/').filter(file => file.endsWith('.js'));
 
 // Handle events depending on their method (once, on)
+
 for (const file of eventFiles) {
-   const event = require(`./events/${file}`);
-   if (event.once) {
-      client.once(event.name, (...args) => event.execute(...args, client));
-   } else {
-      client.on(event.name, (...args) => event.execute(...args, client));
-   }
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
 }
 
 client.login(token);
